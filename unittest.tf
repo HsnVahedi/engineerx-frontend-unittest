4515a4ba06dae08a837a822de99ede52e453c3b1
Hossein Vahedi <mcs.hsn.vahedi@gmail.com>
2021-02-26 03:54:15 +0330

    d

--- unittest.tf
+++ unittest.tf
@@ -26,13 +26,13 @@ resource "kubernetes_pod" "unittest" {
 
       resources {
         limits = {
-          cpu    = "1000m"
-          memory = "256Mi"
+          cpu    = "2000m"
+          memory = "512Mi"
         }
 
         requests = {
-          memory = "128Mi"
-          cpu    = "500m"
+          memory = "256Mi"
+          cpu    = "1000m"
         }
       }
     }
